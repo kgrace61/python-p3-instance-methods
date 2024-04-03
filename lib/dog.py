80216940bbfe66d3f40d7b929454cf060560a2a1
kgrace61 <110522366+kgrace61@users.noreply.github.com>
2024-04-03 12:24:04 -0500

    lab

--- lib/dog.py
+++ lib/dog.py
@@ -3,5 +3,19 @@
 class Dog:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+        
+    def sit(self):
+        print("The dog is sitting.")
+
+fido = Dog()
+fido.bark()
+
+class Person:
+    
+    def talk(self):
+        print("Hello World!")
+        
+    def walk(self):
+        print("The person is walking.")
